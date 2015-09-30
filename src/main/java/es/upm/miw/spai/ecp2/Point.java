e048c688feebb1b5bce12ba3f88e98feed3c9254
RicardoSs <RicardoSs@Ricardo.Home>
2015-10-01 01:41:32 +0200

    Se modifica la clase punto para incluir una tercera coordenada

--- src/main/java/es/upm/miw/spai/ecp2/Point.java
+++ src/main/java/es/upm/miw/spai/ecp2/Point.java
@@ -1,19 +1,20 @@
 package es.upm.miw.spai.ecp2;
 
 public class Point {
-    private int x, y;
+    private int x, y, z;
 
-    public Point(int x, int y) {
+    public Point(int x, int y, int z) {
         this.x = x;
         this.y = y;
+        this.z = z;
     }
 
-    public Point(int xy) {
-        this(xy, xy);
+    public Point(int xyz) {
+        this(xyz, xyz, xyz);
     }
 
     public Point() {
-        this(0, 0);
+        this(0, 0, 0);
     }
 
     public int getX() {
@@ -23,23 +24,28 @@ public class Point {
     public int getY() {
         return this.y;
     }
+    
+    public int getZ() {
+        return this.z;
+    }
 
     public double module() {
-        return Math.sqrt(this.x * this.x + this.y * this.y);
+        return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
     }
 
     public double phase() {
-        return Math.atan((double) this.y / this.x);
+        return Math.atan((double) this.y / this.x / this.z);
     }
 
     public void translateOrigin(Point origin) {
         this.x -= origin.getX();
         this.y -= origin.getY();
+        this.z -= origin.getZ();
     }
 
     @Override
     public String toString() {
-        return "Point[" + x + "," + y + "]";
+        return "Point[" + x + "," + y + "," + z + "]";
     }
     
 }
