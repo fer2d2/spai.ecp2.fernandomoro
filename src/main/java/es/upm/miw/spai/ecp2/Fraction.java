14977463c808cc4d23290b32841fa2556e675152
Raquel Díaz González <raquel.diazgonz@alumnos.upm.es>
2015-10-01 19:40:10 +0200

    Añadidos métodos isMayor() e isMenor()

--- src/main/java/es/upm/miw/spai/ecp2/Fraction.java
+++ src/main/java/es/upm/miw/spai/ecp2/Fraction.java
@@ -1,71 +1,71 @@
 package es.upm.miw.spai.ecp2;
 
 /**
- * Conceptos: Las fracciones propias son aquellas cuyo numerador es menor que el
- * denominador
+ * Conceptos: Las fracciones propias son aquellas cuyo numerador es menor que el denominador
  * 
- * Las fracciones impropias son aquellas cuyo numerador es mayor que el
- * denominador
+ * Las fracciones impropias son aquellas cuyo numerador es mayor que el denominador
  * 
- * Dos fracciones son equivalentes cuando el producto de extremos (numerador de
- * la primera por denominador de la segunda) es igual al producto de medios
- * (denominador de la primera por el numerador de la segunda)
+ * Dos fracciones son equivalentes cuando el producto de extremos (numerador de la primera por denominador de la segunda) es igual al
+ * producto de medios (denominador de la primera por el numerador de la segunda)
  * 
- * Las fracciones irreducibles son aquellas que no se pueden simplificar, esto
- * sucede cuando el numerador y el denominador son primos entre sí
+ * Las fracciones irreducibles son aquellas que no se pueden simplificar, esto sucede cuando el numerador y el denominador son primos entre
+ * sí
  * 
- * Reducir varias fracciones a común denominador consiste en convertirlas en
- * otras equivalentes que tengan el mismo denominador
+ * Reducir varias fracciones a común denominador consiste en convertirlas en otras equivalentes que tengan el mismo denominador
  * 
  * Comparar fracciones
  * 
- * Suma fracciones: En primer lugar se reducen los denominadores a común
- * denominador, y se suman o se restan los numeradores de las fracciones
- * equivalentes obtenidas
+ * Suma fracciones: En primer lugar se reducen los denominadores a común denominador, y se suman o se restan los numeradores de las
+ * fracciones equivalentes obtenidas
  * 
- * Multiplicación: La multiplicación de dos fracciones es otra fracción que
- * tiene: Por numerador el producto de los numeradores. Por denominador el
- * producto de los denominadores.
+ * Multiplicación: La multiplicación de dos fracciones es otra fracción que tiene: Por numerador el producto de los numeradores. Por
+ * denominador el producto de los denominadores.
  * 
- * La división de dos fracciones es otra fracción que tiene: Por numerador el
- * producto de los extremos. Por denominador el producto de los medios. Invertir
- * fraccion
+ * La división de dos fracciones es otra fracción que tiene: Por numerador el producto de los extremos. Por denominador el producto de los
+ * medios. Invertir fraccion
  * 
  * @author jbernal
  * 
  */
 public class Fraction {
-	private int numerator;
+    private int numerator;
 
-	private int denominator;
+    private int denominator;
 
-	public Fraction(int numerator, int denominator) {
-		this.numerator = numerator;
-		this.denominator = denominator;
-	}
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
 
-	public Fraction() {
-		this(1, 1);
-	}
+    public Fraction() {
+        this(1, 1);
+    }
 
-	public int getNumerator() {
-		return numerator;
-	}
+    public int getNumerator() {
+        return numerator;
+    }
 
-	public int getDenominator() {
-		return denominator;
-	}
+    public int getDenominator() {
+        return denominator;
+    }
 
-	public double decimal() {
-		return (double) numerator / denominator;
-	}
+    public double decimal() {
+        return (double) numerator / denominator;
+    }
 
-	public boolean isPropia() {
-		return numerator < denominator;
-	}
+    public boolean isPropia() {
+        return numerator < denominator;
+    }
 
-	public boolean isEquivalente(Fraction candidate) {
-		return ((candidate.numerator == this.numerator) && (candidate.denominator == this.denominator));
-	}
+    public boolean isEquivalente(Fraction candidate) {
+        return ((candidate.numerator == this.numerator) && (candidate.denominator == this.denominator));
+    }
 
+    public boolean isMayor(Fraction candidate) {
+        return this.decimal() > candidate.decimal();
+    }
+
+    public boolean isMenor(Fraction candidate) {
+        return this.decimal() < candidate.decimal();
+    }
 }
